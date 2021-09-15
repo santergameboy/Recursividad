41f9a67a9f93d566f4ed7295b80f1bc169a26223
santergameboy <santer.gameboy@gmail.com>
2021-09-15 09:18:21 -0400

    RECURSIVIDAD

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println(Ejercicios.mcd(412,184));
+        int[] vector = {2,4,2};
+        System.out.println(Ejercicios.sumavector(vector,0));
+    }
+}
